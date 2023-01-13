0d5d707a8b327bf2dd59353f1274888ab94f0903
Karl-Erik Borkmann <karl-erik.borkmann@netgroup.com>
2023-01-13 14:49:14 +0200

    initial setup

---
+++ apps/external/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'ui';
+import { TextField } from '@mui/material';
+
+export default function External() {
+  return (
+    <div>
+      <h1>External</h1>
+      <TextField variant="outlined" />
+      <Button />
+    </div>
+  );
+}
---
+++ packages/ui/Button.tsx
@@ -0,0 +1,5 @@
+import { Button as MuiButton } from '@mui/material';
+
+export const Button = () => {
+  return <MuiButton>Mui button test dfbndscfkjsdhfjkds</MuiButton>;
+};
